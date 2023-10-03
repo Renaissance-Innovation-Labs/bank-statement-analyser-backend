7f79034d8c3bf7fd90b5547f1efe8221712540fe
Cedar Daniel <cedardaniel1998@gmail.com>
2023-10-03 17:53:50 +0100

    new apikey

--- openai.js
+++ openai.js
@@ -2,7 +2,9 @@ const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-  organization: process.env.OPENAI_ORGANIZATION,
+  // organization: process.env.OPENAI_ORGANIZATION,
 });
 
-module.exports = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
+
+module.exports = openai;
